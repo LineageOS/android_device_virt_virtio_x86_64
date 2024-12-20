677981fb138014341b0a5130a030d08214ab9771
Yumi Yukimura <me.cafebabe@gmail.com>
2024-12-21 04:09:24 +0800

    [DNM] virtio_x86_64: Add proprietary-files (15 QPR1)
    Change-Id: I6858c3f145bdca2ef2533fd04d1cc72cc0493e9b

--- device.mk
+++ device.mk
@@ -14,6 +14,39 @@ $(call inherit-product, device/virt/virtio-common/device-common.mk)
 
 DEVICE_PATH := device/virt/virtio_x86_64
 
+# Google Apps
+PRODUCT_PACKAGES += \
+    GmsOverlay \
+    GmsSettingsOverlay \
+    GmsSettingsProviderOverlay \
+    GmsSetupWizardOverlay
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.setupwizard.rotation_locked=true \
+    setupwizard.theme=glif_v3_light \
+    ro.opa.eligible_device=true \
+    ro.com.google.gmsversion=14_beta
+
+# Native bridge
+include frameworks/libs/native_bridge_support/native_bridge_support.mk
+
+PRODUCT_SOONG_NAMESPACES += \
+    frameworks/libs/native_bridge_support/libc
+
+PRODUCT_PACKAGES += \
+    $(NATIVE_BRIDGE_PRODUCT_PACKAGES) \
+    libberberis_exec_region
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.dalvik.vm.native.bridge=libndk_translation.so \
+    ro.dalvik.vm.isa.arm64=x86_64 \
+    ro.dalvik.vm.isa.arm=x86 \
+    ro.enable.native.bridge.exec=1 \
+    ro.ndk_translation.version=0.2.3
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
+
+# Inherit from proprietary files
+$(call inherit-product, vendor/virt/virtio_x86_64/virtio_x86_64-vendor.mk)
