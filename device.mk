d4f4d33e8d1d1522031da048ed69f1f34f253cd6
Yumi Yukimura <me.cafebabe@gmail.com>
2025-01-13 01:39:17 +0800

    virtio_x86_64: Add support for Intel Pre-Broadwell GPU passthrough
    * Broadwell and later uses `iris` gallium driver in Mesa,
      which requires additional build dependencies.
    * "avoid using alpha bits for the framebuffer" [1] commits fixes
      no display after bootanimation on some old Intel GPUs
    
    [1]: https://github.com/android-generic/external_drm_hwcomposer/commits/5be957e457417ee1690480014564fd9d65e31888
    
    Change-Id: Id946b53f22ec60514472b8209dab899aad0b6162

--- device.mk
+++ device.mk
@@ -14,6 +14,21 @@ $(call inherit-product, device/virt/virtio-common/device-common.mk)
 
 DEVICE_PATH := device/virt/virtio_x86_64
 
+# Graphics (Gralloc)
+ifneq ($(TARGET_USES_HWCOMPOSER_DRM_MINIGBM),true)
+PRODUCT_PACKAGES += \
+    android.hardware.graphics.allocator-service.minigbm_intel \
+    android.hardware.graphics.mapper@4.0-impl.minigbm_intel \
+    mapper.minigbm_intel
+endif
+
+PRODUCT_PACKAGES += \
+    gralloc.minigbm_intel
+
+# Init
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/init/virtio_x86_64-graphics.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/virtio_x86_64-graphics.rc
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(DEVICE_PATH)
