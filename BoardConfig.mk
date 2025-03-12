66c131f0a0ebe086f32ef4fca08b9ce7fa641c03
Yumi Yukimura <me.cafebabe@gmail.com>
2025-03-13 07:16:22 +0800

    virtio_x86_64: Add prebuilts/mesa-build-dep to intel_clc search path
    Change-Id: I5817e9c5ee9f54055292ebe3087c48f8ea9a2868

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_GRUB_BOOT_CONFIGS += $(DEVICE_PATH)/bootmgr/grub/grub-boot.cfg
 # Graphics (Mesa)
 BOARD_MESA3D_GALLIUM_DRIVERS += crocus
 
-ifneq ($(wildcard prebuilts/intel-clc/intel_clc),)
+ifneq ($(wildcard prebuilts/intel-clc/intel_clc prebuilts/mesa-build-dep/bin/intel_clc),)
 BOARD_MESA3D_MESON_ARGS += -Dintel-clc=system
 BOARD_MESA3D_GALLIUM_DRIVERS += iris
 BOARD_MESA3D_VULKAN_DRIVERS += intel intel_hasvk
