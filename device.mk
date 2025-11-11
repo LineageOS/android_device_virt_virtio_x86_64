2bb1f0bcde3e07feeaadb34a8356fc8ca8f2132e
Yumi Yukimura <me.cafebabe@gmail.com>
2025-11-11 13:09:08 +0800

    virtio_x86_64: Move the variables before inherit-product down on device.mk
    inherit-product doesn't include the makefile immediately, but appends
    it to the include stack, so the variables gets set at anywhere on
    the current makefile will be available on the inherited makefile too
    
    Change-Id: I4136ffd4d8df376b335feb4c6236584c62b90265

--- device.mk
+++ device.mk
@@ -3,12 +3,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Graphics (Composer)
-TARGET_DRM_HWCOMPOSER_VARIANT := upstream
-
-# Kernel (Prebuilt)
-TARGET_PREBUILT_KERNEL_ARCH := x86_64
-
 # Inherit from common
 $(call inherit-product, device/virt/virtio-common/device-common.mk)
 
@@ -20,9 +14,15 @@ PRODUCT_PACKAGES += \
     gralloc.minigbm_intel \
     mapper.minigbm_intel
 
+# Graphics (Composer)
+TARGET_DRM_HWCOMPOSER_VARIANT := upstream
+
 # Init
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/init/virtio_x86_64-graphics.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/virtio_x86_64-graphics.rc
+
+# Kernel (Prebuilt)
+TARGET_PREBUILT_KERNEL_ARCH := x86_64
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
